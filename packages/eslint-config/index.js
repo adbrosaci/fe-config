7571f09defa0a0fbe8ea935086f9faf38bb52c23
Pavel Pivoda <pavel.pivoda@adbros.cz>
2024-07-22 17:50:02 +0200

    feat(eslint): ignore .gitignore files

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -2,11 +2,14 @@ import globals from 'globals';
 import coreRules from './rules/core.js';
 import unicornRules from './rules/unicorn.js';
 import stylisticRules from './rules/stylistic.js';
-
 import pluginStylistic from '@stylistic/eslint-plugin';
 import pluginUnicorn from 'eslint-plugin-unicorn';
+import gitignore from 'eslint-config-flat-gitignore';
 
 export default [
+	gitignore({
+		strict: false,
+	}),
 	{
 		languageOptions: {
 			globals: globals.browser,
