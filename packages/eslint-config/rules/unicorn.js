496e0e39988aa24615d9ab81a75d74cf96d0ca6a
Pavel Pivoda <pavel.pivoda@adbros.cz>
2025-05-20 06:06:08 +0200

    feat(eslint-config): ignore unicorn/filename-case when brackets are present

--- packages/eslint-config/rules/unicorn.js
+++ packages/eslint-config/rules/unicorn.js
@@ -14,6 +14,7 @@ export default {
 			kebabCase: true,
 			pascalCase: true,
 		},
+		ignore: [ /.*\[[^\]]+].*\.vue$/ ],
 	} ],
 	'unicorn/import-style': 'off',
 	'unicorn/new-for-builtins': 'error',
