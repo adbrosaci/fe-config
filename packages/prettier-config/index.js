583451dca661783d422461a730c58884eed0e0d8
Pavel Pivoda <pavel.pivoda@adbros.cz>
2025-10-21 09:46:16 +0200

    feat(prettier-config): add prettier-plugin-css-order

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -7,7 +7,7 @@ const config = {
 	quoteProps: "as-needed",
 	trailingComma: "all",
 	tabWidth: 2,
-	plugins: ["prettier-plugin-tailwindcss"],
+	plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-css-order"],
 };
 
 export default config;
