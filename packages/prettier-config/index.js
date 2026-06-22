679ff493a952cb4a7cb6f39dc004964a03f72bc4
Pavel Pivoda <pavel.pivoda@adbros.cz>
2026-06-22 12:51:33 +0200

    feat(prettier-config): do not assume editorconfig settings and set them explicitly in prettier config

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -8,6 +8,16 @@ const config = {
 	trailingComma: "all",
 	tabWidth: 2,
 	plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss", "prettier-plugin-css-order"],
+	useTabs: true,
+	endOfLine: "lf",
+	overrides: [
+		{
+			files: ["*.json", "*.yml", "*.yaml"],
+			options: {
+				useTabs: false,
+			},
+		},
+	],
 };
 
 export default config;
